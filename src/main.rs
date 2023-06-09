de0df572d4569a29905712ccc8d89c63b293618c
Hiroaki Nakamura <hnakamur@gmail.com>
2023-06-09 23:30:46 +0900

    Use minijinja::Source to load other template files

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{
-    fs::File,
-    io::{self, Read, Write},
+    env::current_dir,
+    fs::{self, File},
+    io::Write,
     path::Path,
 };
 
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
-use minijinja::Environment;
+use minijinja::{Environment, Error, ErrorKind, Source};
 use serde_yaml::Value;
 
 #[derive(Parser, Debug)]
@@ -24,20 +26,39 @@ struct Args {
     dest: String,
 }
 
-type DynError = Box<dyn std::error::Error + Send + Sync + 'static>;
-
-fn main() -> Result<(), DynError> {
+fn main() -> Result<()> {
     let args = Args::parse();
 
-    let template_path = &args.tmpl;
+    let template_path = current_dir()?.join(&args.tmpl);
     let yaml_path = &args.var;
 
     let mut env = Environment::new();
     env.set_undefined_behavior(minijinja::UndefinedBehavior::Strict);
 
-    let template_contents = read_file_to_string(template_path)?;
-    env.add_template(template_path, &template_contents)?;
-    let template = env.get_template(template_path)?;
+    let template_dir = template_path.parent().unwrap().to_owned();
+    env.set_source(Source::with_loader(move |name| {
+        let path = template_dir.join(name);
+
+        match fs::read_to_string(path) {
+            Ok(result) => Ok(Some(result)),
+            Err(err) => {
+                let msg = if err.kind() == std::io::ErrorKind::NotFound {
+                    "template not found"
+                } else {
+                    "failed to load template"
+                };
+                Err(Error::new(ErrorKind::TemplateNotFound, msg).with_source(err))
+            }
+        }
+    }));
+
+    let template_file = template_path
+        .as_path()
+        .file_name()
+        .ok_or(anyhow!("invalid template path"))?
+        .to_str()
+        .ok_or(anyhow!("not UTF-8 template path"))?;
+    let template = env.get_template(template_file)?;
 
     let yaml_data = read_yaml_file(yaml_path)?;
     let rendered_contents = template.render(yaml_data)?;
@@ -50,24 +71,7 @@ fn main() -> Result<(), DynError> {
     Ok(())
 }
 
-fn read_yaml_file<P: AsRef<Path>>(file_path: P) -> Result<Value, serde_yaml::Error> {
-    let contents = read_file_to_string(file_path).expect("open or read yaml file");
-    serde_yaml::from_str(&contents)
-}
-
-fn read_file_to_string<P: AsRef<Path>>(file_path: P) -> Result<String, io::Error> {
-    let mut file = File::open(&file_path).unwrap_or_else(|_| {
-        panic!(
-            "unable to open file: {}",
-            file_path.as_ref().to_string_lossy()
-        )
-    });
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap_or_else(|_| {
-        panic!(
-            "unable to read file: {}",
-            file_path.as_ref().to_string_lossy()
-        )
-    });
-    Ok(contents)
+fn read_yaml_file<P: AsRef<Path>>(file_path: P) -> Result<Value> {
+    let contents = fs::read_to_string(file_path)?;
+    serde_yaml::from_str(&contents).context("load variables in YAML")
 }
